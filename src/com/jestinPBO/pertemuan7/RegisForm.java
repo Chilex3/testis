d86b54de7fffd17389327ea5567e1641b44313be
Jestin Reynard <jestinpolres123@gmail.com>
2023-06-07 14:29:26 +0700

    pertemuan 7 GUI

--- src/com/jestinPBO/pertemuan7/RegisForm.java
+++ src/com/jestinPBO/pertemuan7/RegisForm.java
@@ -1,11 +1,13 @@
 package com.jestinPBO.pertemuan7;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RegisForm extends JFrame{
     private JTextField txtNama;
     private JTextField textNim;
-    private JComboBox comboBox1;
+    private JComboBox Cprodi;
     private JTextField noTelp;
     private JRadioButton priaRadioButton;
     private JRadioButton wanitaRadioButton;
@@ -20,7 +22,42 @@ public class RegisForm extends JFrame{
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.setSize(800,600);
+
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtNama.setText("");
+                textNim.setText("");
+                noTelp.setText("");
+                Alamat.setText("");
+                Cprodi.setSelectedIndex(0);
+                priaRadioButton.setSelected(false);
+                wanitaRadioButton.setSelected(false);
+            }
+        });
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nama = txtNama.getText();
+                String nim = textNim.getText();
+                String prodi = (String) Cprodi.getSelectedItem();
+                String telp = noTelp.getText();
+                String alamat = Alamat.getText();
+                String JK;
+                if(priaRadioButton.isSelected()){
+                    JK = "Pria";
+                } else if (wanitaRadioButton.isSelected()) {
+                    JK = "Wanita";
+                } else {
+                    JK = "Tidak ditemukan";
+                }
+
+                JOptionPane.showMessageDialog(null,"Data berhasil disimpan!");
+            }
+        });
     }
+
     public static void main(String[] args){
         JFrame mainFrame = new RegisForm();
         mainFrame.setVisible(true);
